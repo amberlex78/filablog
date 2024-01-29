a8e3ac6f55a2550d339a73cff05ebef80de65b9c
Oleksii Abrosymov <amberlex78@gmail.com>
2024-01-29 12:14:17 +0200

    Fixed create in seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,8 +12,8 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            [
+        User::factory(2)
+            ->sequence([
                 'name' => 'Admin',
                 'email' => 'admin@example.com',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -21,8 +21,8 @@ class UserSeeder extends Seeder
                 'name' => 'User',
                 'email' => 'user@example.com',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            ],
-        ]);
+            ])
+            ->create();
 
         User::factory(8)->create();
     }
